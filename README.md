e28cde23f2f1505819d7a06560e1bf26a99af17a
Natalia Pavlova <pavlova_li@ukr.net>
2023-02-08 17:56:29 +0200

    currency-converter

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # currency-converter
+
+[!1](./images/screen-one.png)
+[!2](./images/screen-two.png)
+
+Currency converter, using pure JavaScript + free API.
+
+## references
+
+API for currency - https://www.exchangerate-api.com/
+
+[DESIGN IN FIGMA](https://www.figma.com/file/rrh9K7I0wXjdSR8JoUYCtV/Currency-Exchange-App?t=tBD2TGzFETayXyFr-0)
---
+++ js/state.js
@@ -0,0 +1,19 @@
+export default {
+  url: 'https://v6.exchangerate-api.com/v6/c8ac6b272f519b29f3857cb6',
+  codes: [],
+  pair: {
+    from: '',
+    to: '',
+  },
+  amount: '',
+  loading: false,
+  currentTab: 'convert',
+  currency: {
+    code: 'USD',
+  },
+  currencies: ['USD', 'EUR', 'BYN'],
+  actions: {
+    remove: 'remove',
+    change: 'change',
+  },
+}
